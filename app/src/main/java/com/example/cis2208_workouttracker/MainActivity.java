817536dc852a92af3a53ddbf62a4406c69f5f364
DanielFarrugiaUM <82520234+DanielFarrugiaUM@users.noreply.github.com>
2022-05-22 17:08:03 +0200

    added non-functional settings screen

--- app/src/main/java/com/example/cis2208_workouttracker/MainActivity.java
+++ app/src/main/java/com/example/cis2208_workouttracker/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import com.example.cis2208_workouttracker.domainModels.Exercise;
 import com.example.cis2208_workouttracker.domainModels.RepExercise;
 import com.example.cis2208_workouttracker.domainModels.TimedExercise;
+import com.example.cis2208_workouttracker.ui.settings.SettingsActivity;
 import com.example.cis2208_workouttracker.ui.workouts.AddWorkoutActivity;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
@@ -28,6 +30,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    MaterialToolbar topAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +39,25 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        topAppBar = (MaterialToolbar) findViewById(R.id.topAppBar);
+        setSupportActionBar(topAppBar);
+
+
+        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = binding.viewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+
+
         //FloatingActionButton fab = binding.fab;
 
 /*        fab.setOnClickListener(new View.OnClickListener() {
@@ -51,5 +68,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });*/
 
+    }
+    //Set up a menu from the relevant xml
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_app_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //Set up the menu actions
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
