50c48c562356a90fb5722aea0a1b4e0f8ec831de
DanielFarrugiaUM <82520234+DanielFarrugiaUM@users.noreply.github.com>
2022-05-22 15:42:18 +0200

    changed add workout button to be disabled if no test is in input

--- app/src/main/java/com/example/cis2208_workouttracker/ui/workouts/AddWorkoutActivity.java
+++ app/src/main/java/com/example/cis2208_workouttracker/ui/workouts/AddWorkoutActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -19,19 +21,42 @@ import com.example.cis2208_workouttracker.domainModels.Workout;
 public class AddWorkoutActivity extends AppCompatActivity {
     private ActivityAddWorkoutBinding binding;
 
+    private EditText workoutNameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_workout);
         getSupportActionBar().setTitle(R.string.add_workout);
+
         //Get the button from layout and set listener
         Button confirmBtn = findViewById(R.id.confirm_btn);
         confirmBtn.setOnClickListener(this::onClickConfirm);
+
+        //Get the name view and disable confirm button
+        workoutNameView = findViewById(R.id.workout_name);
+        workoutNameView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //Set to true if the length of the input is not 0
+                confirmBtn.setEnabled(!charSequence.toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
 
     public void onClickConfirm(View view){
         //Find and get the view with the user's input and convert it to string
-        EditText workoutNameView = findViewById(R.id.workout_name);
+
         String workoutName = workoutNameView.getText().toString();
         Workout workout = new Workout(workoutName);
         //Get the tools to write to DB and save the new workout
